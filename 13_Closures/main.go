4df9f344b5379921ec302c3d60074ca7adc2ee79
vedikhub <vedikcodes@gmail.com>
2025-04-02 01:35:36 +0530

    CLosure functions in Golang

---
+++ 13_Closures/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+//Go support anonymous functions
+//useful when you want to define a afunction inline without having to name it
+
+func intSeq() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
+
+func main() {
+	nextInt := intSeq()
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+
+	newInts := intSeq()
+	fmt.Println(newInts())
+
+}
